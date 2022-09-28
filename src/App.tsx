bfe0e26f49b0ffdb6f5a49fb7773d2a1086a004d
Matheus <etec.matheus@gmail.com>
2022-09-28 00:44:05 -0300

    adding home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>teste</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
