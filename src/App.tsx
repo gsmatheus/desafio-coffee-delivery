497f744c551619e708cd9bd59ad710c53a684c11
Matheus <etec.matheus@gmail.com>
2022-09-28 00:35:05 -0300

    adding default styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return <div>app</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>teste</h1>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
